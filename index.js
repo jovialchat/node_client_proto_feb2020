2523a6059622c4ba3b23937bd3f72be5f8ca8808
Rishav Bhowmik <bhowmikrishav@gmail.com>
2021-03-07 01:23:19 +0530

    bsonize

--- index.js
+++ index.js
@@ -4,20 +4,17 @@ exports.__esModule = true;
 var TimeStamp_1 = require("./src/TimeStamp");
 var Message_1 = require("./src/Message");
 var MessageLine_1 = require("./src/MessageLine");
-var MessageLineImage_1 = require("./src/MessageLine/MessageLineImage");
+var bson_1 = require("bson");
 function main() {
     var message = new Message_1.Message({
         sender_id: "send123",
         reciver_id: "reci321",
         timestamp: new TimeStamp_1.TimeStamp({}),
         body: [
-            new MessageLine_1.MessageLineTextPlain("Hellow"),
-            new MessageLineImage_1.MessageLineImage({
-                file_name: 'hellowkn;kwef/wef/we/fwefewfnjnfewnf',
-                file_type: 'image/png'
-            })
+            new MessageLine_1.MessageLineTextPlain("Hello World"),
         ]
     });
     console.log(message.toObject().body);
+    console.log(bson_1.deserialize(bson_1.serialize(message.toObject())).body);
 }
 main();
--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import { TimeStamp } from "./src/TimeStamp";
 import { Message } from "./src/Message";
 import { MessageLineTextPlain } from "./src/MessageLine";
 import { MessageLineImage } from "./src/MessageLine/MessageLineImage";
+import { serialize, deserialize } from 'bson'
 
 function main() {
     const message = new Message({
@@ -10,16 +11,22 @@ function main() {
         reciver_id: "reci321",
         timestamp: new TimeStamp({}),
         body: [
-            new MessageLineTextPlain("Hellow"),
-            new MessageLineImage({
-                file_name: 'hellowkn;kwef/wef/we/fwefewfnjnfewnf',
-                file_type: 'image/png'
-            })
+            new MessageLineTextPlain("Hello World"),
+            // new MessageLineImage({
+            //     file_name: 'hellowkn',
+            //     file_key: 'hellowkn;kwef/wef/we/fwefewfnjnfewnf',
+            //     file_type: 'image/png'
+            // })
         ]
     })
     console.log(
         message.toObject().body
     );
+    console.log(
+        deserialize(
+            serialize(message.toObject())
+        ).body
+    );   
 }
 
 main()
